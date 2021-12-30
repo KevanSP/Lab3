da154cc2b433a423a60fddc559eea3f230b67a06
Christopher Sherman <christopher.sherman.isa@gmail.com>
2021-12-30 16:23:14 +0800

    Initial Revision

---
+++ Lab3.py
@@ -0,0 +1,44 @@
+print("Lab 3 - Software Unit Testing with PyTest")
+
+SORT_ASCENDING = 0
+SORT_DESCENDING = 1
+
+
+def bubbleSort(arr, sorting_order):
+
+    # Copy input list to results list
+    arr_result = arr.copy()
+
+    # Get number of elements in the list
+    n = len(arr_result)
+
+    # Traverse through all array elements
+    for i in range(n - 1):
+        # range(n) also work but outer loop will
+        # repeat one time more than needed.
+
+        # Last i elements are already in place
+        for j in range(0, n - i - 1):
+
+            if sorting_order == SORT_ASCENDING:
+                if arr_result[j] > arr_result[j + 1]:
+                    arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
+
+
+            elif sorting_order == SORT_DESCENDING:
+                if arr_result[j] < arr_result[j + 1]:
+                    arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
+
+
+
+    return arr_result
+
+# Driver code to test above
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+result = bubbleSort(arr, SORT_ASCENDING)
+
+print("Sorted array is:")
+
+for i in range(len(result)):
+    print("% d" % result[i])
