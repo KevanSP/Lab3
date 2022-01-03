40d462efdf54181dd6da995405cb9f735e1f859a
Chris <christopher.sherman.isa@gmail.com>
2022-01-03 18:07:34 +0800

    Add PyTest test cases for Lab 3

--- Lab3.py
+++ Lab3.py
@@ -12,24 +12,29 @@ def bubble_sort(arr, sorting_order):
     # Get number of elements in the list
     n = len(arr_result)
 
-    # Traverse through all array elements
-    for i in range(n - 1):
-        # range(n) also work but outer loop will
-        # repeat one time more than needed.
+    if n < 10:
+        # Traverse through all array elements
+        for i in range(n - 1):
+            # range(n) also work but outer loop will
+            # repeat one time more than needed.
 
-        # Last i elements are already in place
-        for j in range(0, n - i - 1):
+            # Last i elements are already in place
+            for j in range(0, n - i - 1):
 
-            if sorting_order == SORT_ASCENDING:
-                if arr_result[j] > arr_result[j + 1]:
-                    arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
-
-
-            elif sorting_order == SORT_DESCENDING:
-                if arr_result[j] < arr_result[j + 1]:
-                    arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
+                if sorting_order == SORT_ASCENDING:
+                    if arr_result[j] > arr_result[j + 1]:
+                        arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
 
 
+                elif sorting_order == SORT_DESCENDING:
+                    if arr_result[j] < arr_result[j + 1]:
+                        arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
+
+                else:
+                    # Return an empty array
+                    arr_result = []
+        else:
+            arr_result = -1
 
     return arr_result
 
--- Test_Lab3.py
+++ Test_Lab3.py
@@ -1,5 +1,30 @@
+import Lab3
+
 print("Test_Lab3")
 
 
-def test_bubble_sort():
-    assert False
+def test_bubble_sort_ascending():
+    result = []
+    input_arr = [64, 34, 25, 12, 22, 11, 90]
+    test_arr = [11, 12, 22, 25, 34, 64, 90]
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == test_arr)
+
+def test_bubble_sort_descending():
+    result = []
+    input_arr = [64, 34, 25, 12, 22, 11, 90]
+    test_arr = [90, 64, 34, 25, 22, 12, 11]
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
+
+    assert (result == test_arr)
+
+def test_bubble_sort_invalid():
+    result = []
+    input_arr = [64, 34, 25, 12, 22, 11, 90]
+
+    result = Lab3.bubble_sort(input_arr, 3)
+
+    assert (result == [])
